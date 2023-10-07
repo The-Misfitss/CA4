643421d641608799f20b922c07e52a998ed320dc
Muhammad Abdullah Cheema <notabdullah1025@gmail.com>
2023-10-07 23:41:01 +0500

    Changed the schema of db to what hammad has defined

--- init.sql
+++ init.sql
@@ -1,25 +1,16 @@
--- Create a new database for users
-CREATE DATABASE userdb;
-
--- Switch to the new database
-\c userdb;
-
--- Create a table to store user information
-CREATE TABLE users (
+-- Create a table called students in the mydb database
+CREATE TABLE students (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    pass VARCHAR(255) NOT NULL
+    name VARCHAR(255),
+    rollnumber VARCHAR(255)
 );
 
--- Insert some sample user data
-INSERT INTO users (username, email, pass)
-VALUES
-    ('user1', 'user1@example.com', 'password1'),
-    ('user2', 'user2@example.com', 'password2'),
-    ('user3', 'user3@example.com', 'password3');
+-- Insert name and roll numbers into the student table
+INSERT INTO students (name, rollnumber) VALUES ('John', '1');
+INSERT INTO students (name, rollnumber) VALUES ('Doe', '2');
+INSERT INTO students (name, rollnumber) VALUES ('Jane', '3');
+INSERT INTO students (name, rollnumber) VALUES ('Doe', '4');
+INSERT INTO students (name, rollnumber) VALUES ('John', '5');
 
--- Commit the transaction
-COMMIT;
-
-Select * from users;
+-- Select all rows from the students table
+SELECT * FROM students;
